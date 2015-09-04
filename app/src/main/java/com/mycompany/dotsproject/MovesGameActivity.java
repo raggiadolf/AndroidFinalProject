c4d008a6008ac84046b44eedb15cfca936aad01f
ragnaradolf <ragnar.adolf@gmail.com>
2015-09-04 12:22:11 +0000

    Started adding front page

--- app/src/main/java/com/mycompany/dotsproject/MovesGameActivity.java
+++ app/src/main/java/com/mycompany/dotsproject/MovesGameActivity.java
@@ -1,24 +1,22 @@
 package com.mycompany.dotsproject;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MovesGameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_moves_game);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_moves_game, menu);
         return true;
     }
 
@@ -35,10 +33,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void startMovesGame(View view) {
-        Intent intent = new Intent(this, MovesGameActivity.class);
-        startActivity(intent);
     }
 }
