5f68194ef3e216f2f9613ba66ceb8991673ca8e8
ragnaradolf <ragnar.adolf@gmail.com>
2015-09-15 17:26:20 +0000

    emptyRecords now clears both set of records, six and eight

--- app/src/main/java/com/mycompany/dotsproject/DotsPreferenceActivity.java
+++ app/src/main/java/com/mycompany/dotsproject/DotsPreferenceActivity.java
@@ -1,6 +1,5 @@
 package com.mycompany.dotsproject;
 
-import android.app.ActionBar;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,7 +7,6 @@ import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
-import android.widget.TextView;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -48,9 +46,14 @@ public class DotsPreferenceActivity extends PreferenceActivity {
 
     private void emptyRecords() {
         try {
-            FileOutputStream fos = openFileOutput("records.ser", Context.MODE_PRIVATE);
-            ObjectOutputStream oos = new ObjectOutputStream(fos);
-            oos.writeObject(new ArrayList<Record>());
+            FileOutputStream fossix = openFileOutput("recordssix.ser", Context.MODE_PRIVATE);
+            FileOutputStream foseight = openFileOutput("recordseight.ser", Context.MODE_PRIVATE);
+
+            ObjectOutputStream oossix = new ObjectOutputStream(fossix);
+            oossix.writeObject(new ArrayList<Record>());
+
+            ObjectOutputStream ooseight = new ObjectOutputStream(foseight);
+            ooseight.writeObject(new ArrayList<Record>());
         } catch(IOException ex) {
             // TODO: Handler exception
         }
--- app/src/main/java/com/mycompany/dotsproject/HighScoreActivity.java
+++ app/src/main/java/com/mycompany/dotsproject/HighScoreActivity.java
@@ -6,10 +6,8 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.EditText;
 import android.widget.ListView;
 
 import java.io.FileInputStream;
