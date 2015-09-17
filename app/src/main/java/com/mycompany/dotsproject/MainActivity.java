07b53a79ec6082b5babd3ab0e78cf2d1621b7359
ragnaradolf <ragnar.adolf@gmail.com>
2015-09-17 22:42:43 +0000

    Added tooltips to main menu

--- app/src/main/java/com/mycompany/dotsproject/MainActivity.java
+++ app/src/main/java/com/mycompany/dotsproject/MainActivity.java
@@ -3,20 +3,34 @@ package com.mycompany.dotsproject;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private Animation animRotate;
+    private Animation m_animRotate;
+    private Animation m_animAlpha;
+
+    private TextView m_helpTimedGame;
+    private TextView m_helpMovesGame;
+    private TextView m_helpHighScores;
+    private TextView m_helpSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        animRotate = AnimationUtils.loadAnimation(this, R.anim.anim_rotate);
+        m_animRotate       = AnimationUtils.loadAnimation(this, R.anim.anim_rotate);
+        m_animAlpha        = AnimationUtils.loadAnimation(this, R.anim.anim_alpha);
+
+        m_helpTimedGame  = (TextView) findViewById(R.id.helpTimedGame);
+        m_helpMovesGame  = (TextView) findViewById(R.id.helpMovesGame);
+        m_helpHighScores = (TextView) findViewById(R.id.helpHighScores);
+        m_helpSettings   = (TextView) findViewById(R.id.helpSettings);
     }
 
     @Override
@@ -49,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
      * @param view
      */
     public void startMovesGame(View view) {
-        view.startAnimation(animRotate);
+        view.startAnimation(m_animRotate);
         Intent intent = new Intent(this, MovesGameActivity.class);
         startActivity(intent);
     }
@@ -60,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
      * @param view
      */
     public void viewHighScores(View view) {
-        view.startAnimation(animRotate);
+        view.startAnimation(m_animRotate);
         Intent intent = new Intent(this, HighScoreActivity.class);
         startActivity(intent);
     }
@@ -70,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
      * @param view
      */
     public void startTimedGame(View view) {
-        view.startAnimation(animRotate);
+        view.startAnimation(m_animRotate);
         Intent intent = new Intent(this, MovesGameActivity.class);
         intent.putExtra("timed", true);
         startActivity(intent);
@@ -81,8 +95,19 @@ public class MainActivity extends AppCompatActivity {
      * @param view
      */
     public void settings(View view) {
-        view.startAnimation(animRotate);
+        view.startAnimation(m_animRotate);
         Intent intent = new Intent(this, DotsPreferenceActivity.class);
         startActivity(intent);
+    }
+
+    /**
+     * TOOD: Document
+     * @param view
+     */
+    public void displayTooltips(View view) {
+        m_helpTimedGame.startAnimation(m_animAlpha);
+        m_helpMovesGame.startAnimation(m_animAlpha);
+        m_helpHighScores.startAnimation(m_animAlpha);
+        m_helpSettings.startAnimation(m_animAlpha);
     }
 }
